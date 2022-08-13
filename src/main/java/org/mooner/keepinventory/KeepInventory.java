4533c1c7e9f8cb8b6214c8806049dd00367bcc68
Mooner510 <lmck510@gmail.com>
2022-08-14 00:40:04 +0900

    Initial commit

---
+++ src/main/java/org/mooner/keepinventory/KeepInventory.java
@@ -0,0 +1,37 @@
+package org.mooner.keepinventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KeepInventory extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Plugin Enabled!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Plugin Disabled!");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDeath(PlayerDeathEvent e) {
+        e.setKeepInventory(true);
+        if(e.getEntity().isOp()) {
+            e.setKeepLevel(true);
+            return;
+        }
+        e.setDroppedExp(0);
+        e.setNewExp(0);
+        e.setNewLevel(0);
+        e.setNewTotalExp(0);
+    }
+}
