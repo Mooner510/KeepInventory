5c5a7e3d0f71df0de5f57a93bb83845affbd0949
Mooner510 <lmck510@gmail.com>
2022-08-15 14:14:26 +0900

    Added Prevent Exploding

--- src/main/java/org/mooner/keepinventory/KeepInventory.java
+++ src/main/java/org/mooner/keepinventory/KeepInventory.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -34,5 +36,15 @@ public final class KeepInventory extends JavaPlugin implements Listener {
         e.setNewExp(0);
         e.setNewLevel(0);
         e.setNewTotalExp(0);
+    }
+
+    @EventHandler
+    public void onExplode(BlockExplodeEvent e) {
+        e.blockList().clear();
+    }
+
+    @EventHandler
+    public void onExplodeEntity(EntityExplodeEvent e) {
+        e.blockList().clear();
     }
 }
